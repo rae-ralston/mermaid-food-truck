485ca1b3aef33d0c0b77e4e9b1bdd23e942d0a1a
Rae Ralston <rachelralston@gmail.com>
2026-02-14 06:41:44 -0800

    add pickup window that completes orders (#46)
    Co-authored-by: Rachel Ralston <rachel.ralston@kofile.com>

---
+++ scripts/truck_pickup_window.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+signal order_fulfilled(order: StringName)
+
+func interact(actor) -> Dictionary:
+	if not actor.is_holding(): 
+		return {}
+	
+	#just emits that the recipe is complete
+	var recipe_id: StringName = actor.held_item
+	order_fulfilled.emit(recipe_id)
+	actor.held_item = ""
+	
+	return {}
